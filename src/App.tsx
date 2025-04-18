660160d4845e27d81ac5049d52e52ed967b8be29
LakshanRajapaksha01 <rajapaksharpl.22@uom.lk>
2025-04-18 12:46:08 +0530

    Generate Report button & Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { CssBaseline } from '@mui/material';
+import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import theme from "./assets/styles/theme";
+import ReportOverview from "../../Spend-Smart-frontend/src/pages/Report/ReportOverview";
+import Header from "../../Spend-Smart-frontend/src/components/header/header";
 
 function App() {
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Header pageName="Report Overview" />
+      <ReportOverview />
     </ThemeProvider>
-  )
-
+  );
 }
 
 export default App;
