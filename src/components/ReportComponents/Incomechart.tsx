539749dc56c6656db78ca817a97c8a0c2d8f74c1
LakshanRajapaksha01 <rajapaksharpl.22@uom.lk>
2025-04-26 14:43:26 +0530

    Income chart

---
+++ src/components/ReportComponents/Incomechart.tsx
@@ -0,0 +1,50 @@
+import {
+  areaElementClasses,
+  LineChart,
+  lineElementClasses,
+} from "@mui/x-charts/LineChart";
+import { Box } from "@mui/material";
+
+const margin = { right: 24 };
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const xLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+
+export default function BalanceAreaChart() {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: 600,
+        mt: 2,
+        p: 2,
+        boxShadow: 3,
+        borderRadius: 2,
+        backgroundColor: "#fff",
+      }}
+    >
+      <LineChart
+        height={300}
+        series={[
+          {
+            data: uData,
+            label: "Income",
+            area: true,
+            showMark: false,
+            color: "blue",
+          },
+        ]}
+        xAxis={[{ scaleType: "point", data: xLabels }]}
+        yAxis={[{ min: 0, max: 10000 }]}
+        sx={{
+          [`& .${lineElementClasses.root}`]: {
+            stroke: "blue",
+          },
+          [`& .${areaElementClasses.root}`]: {
+            fill: "rgba(255, 0, 111, 0.3)",
+          },
+        }}
+        margin={margin}
+      />
+    </Box>
+  );
+}
--- src/pages/Report/ReportDisplay.tsx
+++ src/pages/Report/ReportDisplay.tsx
@@ -2,32 +2,37 @@ import { Box, Grid } from "@mui/material";
 import Cards from "../../components/ReportComponents/Cards";
 import PieChart from "../../components/ReportComponents/PieChart";
 import BarGraph from "../../components/ReportComponents/BarGraph";
+import BalanceAreaChart from "../../components/ReportComponents/Incomechart";
 
 function ReportDisplay() {
   return (
-    
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          p: 3,
-        }}
-      >
-        <Cards />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        p: 3,
+      }}
+    >
+      <Cards />
+      <Box sx={{ mt: 2 }}>
+        <Grid container spacing={2} sx={{ width: "100%", margin: 0 }}>
+          <Grid item xs={12} md={6}>
+            <PieChart />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <BarGraph />
+          </Grid>
+        </Grid>
         <Box sx={{ mt: 2 }}>
-          <Grid container spacing={2} sx={{ width: "100%", margin: 0 }}>
-           
+          <Grid sx={{ ml: 1.8 }}>
             <Grid item xs={12} md={6}>
-              <PieChart />
-            </Grid>
-       
-            <Grid item xs={12} md={6}>
-              <BarGraph />
+              <BalanceAreaChart />
             </Grid>
           </Grid>
         </Box>
       </Box>
-    
+    </Box>
   );
 }
 
