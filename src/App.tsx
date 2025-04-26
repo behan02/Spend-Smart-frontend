278e2d48023c7bceba8b4a24738a60d5bae47a03
LakshanRajapaksha01 <rajapaksharpl.22@uom.lk>
2025-04-27 01:55:19 +0530

    Add Side Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Sidebar from "./components/sidebar/sidebar";
 import UserLogin from "./pages/UserLogin/UserLogin";
 import UserRegister from "./pages/UserRegister/UserRegister";
 
-
-
-
-
-
 function App() {
   return (
-  
-    
-
-      <Router>
-        <Routes>
-
-          <Route path="/" element={<UserLogin/>} />
-          <Route path="/register" element={<UserRegister/>} /> {/* Add the route for Goals page */}
-
-        </Routes>
-      </Router>
- 
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<UserLogin />} />
+        <Route path="/register" element={<UserRegister />} />{" "}
+        {/* Add the route for Goals page */}
+      </Routes>
+    </Router>
   );
 }
 
