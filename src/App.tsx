496089af4c6e0534869e339020f9e1d9be3d6a06
LakshanRajapaksha01 <rajapaksharpl.22@uom.lk>
2025-04-29 11:29:16 +0530

    Add footer Generate Report page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReportDisplay from "./pages/Report/ReportDisplay";
 import ReportOverview from "./pages/Report/ReportOverview";
 import Sidebar from "./components/sidebar/sidebar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<ReportOverview />} />
         <Route path="/reportdisplay" element={<ReportDisplay />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
