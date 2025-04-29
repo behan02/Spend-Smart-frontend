d84cae70f92fc43e80e055cedb4eab6d9b898219
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-04-29 11:13:13 +0530

    add admin login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/sidebar/sidebar";
+
 import UserLogin from "./pages/UserLogin/UserLogin";
 import UserRegister from "./pages/UserRegister/UserRegister";
+import AdminLogin from "./pages/AdminLogin.tsx/AdminLogin";
 
 function App() {
   return (
     <Router>
-      <Sidebar />
+      
       <Routes>
-        <Route path="/" element={<UserLogin />} />
+        <Route path="/" element={<AdminLogin />} />
         <Route path="/register" element={<UserRegister />} />{" "}
         {/* Add the route for Goals page */}
       </Routes>
