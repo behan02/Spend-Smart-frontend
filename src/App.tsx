68498837e3f1c0b0c4e3fbb1ee9f2acd78d3fe9e
saknarajapakshe <rajapaksherars.22@uom.lk>
2025-05-01 07:58:52 +0530

    Remove unused imports from App component to streamline code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/sidebar/sidebar";
-import UserLogin from "./pages/UserLogin/UserLogin";
-import UserRegister from "./pages/UserRegister/UserRegister";
 import Goals from "./pages/Goals/Goals";
 import { Box } from "@mui/material";
-import { useState } from 'react'
         
 function App() {
   return(
