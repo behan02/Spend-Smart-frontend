fe537f7e9b0b3b33bb34ee137fd1927c34ffe920
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-05-01 16:45:07 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ReportDisplay from "./pages/Report/ReportDisplay";
+
 import ReportOverview from "./pages/Report/ReportOverview";
-import Sidebar from "./components/sidebar/sidebar";
-import Footer from "./components/footer/Footer";
+
+import ReportGenerate from "./pages/Report/ReportGenerate";
 
 function App() {
   return (
     <Router>
-      <Sidebar />
+  
       <Routes>
         <Route path="/" element={<ReportOverview />} />
-        <Route path="/reportdisplay" element={<ReportDisplay />} />
+        <Route path="/reportGenerate" element={<ReportGenerate />} />
       </Routes>
-      <Footer />
+     
     </Router>
   );
 }
--- src/components/ReportOverview/GenerateReport.tsx
+++ src/components/ReportOverview/GenerateReport.tsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
 
 
 function GenerateReport() {
+  const navigate = useNavigate()
+  const handleGenerateReport = () => {
+    // Logic to generate the report
+    // For example, you might want to fetch data from an API or perform some calculations
+    console.log("Generating report...")
+    // After generating the report, navigate to the ReportDisplay page
+    navigate("/reportGenerate")
+  }
   return (
-    <Button
-  variant="contained"
-  color="primary"
-  sx={{
-    mt: 2,
-    borderRadius: "10px",
-    px: { xs: 2, sm: 3 },
-    py: { xs: 1, sm: 1.5 },
-    fontSize: { xs: ".5rem", md: ".9rem" } // Set directly here
-  }}
->
-  Generate Report
-</Button>
+<Button
+      variant="contained"
+      color="primary"
+      onClick={handleGenerateReport}
+      sx={{
+        mt: 2,
+        borderRadius: "10px",
+        px: { xs: 2, sm: 3 },
+        py: { xs: 1, sm: 1.5 },
+        fontSize: { xs: ".5rem", md: ".9rem" } // Set directly here
+      }}
+    >
+      Generate Report
+    </Button>
 
   )
 }
