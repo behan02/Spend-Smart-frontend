432fc61987297761459f537e61e19cad961ed05e
LakshanRajapaksha01 <rajapaksharpl.22@uom.lk>
2025-05-01 18:40:09 +0530

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
-import ReportDisplay from "./pages/Report/ReportDisplay";
-import ReportOverview from "./pages/Report/ReportOverview";
-import Sidebar from "./components/sidebar/sidebar";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Transaction from "./pages/Transaction/Transaction";
-import Goals from "./pages/Goals";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import theme from "./assets/styles/theme";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserSettings from "./pages/UserSettings/UserSettings";
+import Sidebar from "../src/components/sidebar/sidebar";
+import Footer from "../src/components/footer/Footer";
 
 function App() {
   return (
-    // <Router>
-    //   <Sidebar />
-    //   <Routes>
-    //     <Route path="/" element={<ReportOverview />} />
-    //     <Route path="/reportdisplay" element={<ReportDisplay />} />
-    //   </Routes>
-    //   <Footer />
-    // </Router>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/report" element={<ReportOverview />} />
-          <Route path="/goals" element={<Goals />} />
-          <Route path="/transaction" element={<Transaction />} />
-          <Route path="/settings" element={<UserSettings />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <Router>
+      <div style={{ display: "flex", flexDirection: "row", minHeight: "100vh" }}>
+        {/* Sidebar */}
+        <Sidebar />
+
+        {/* Main content */}
+        <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <div style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<UserSettings />} />
+            </Routes>
+          </div>
+
+          {/* Footer */}
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
