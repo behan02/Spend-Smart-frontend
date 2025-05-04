9e6bd292f66ff5c9acb76f111640b66f070a9407
LakshanRajapaksha01 <rajapaksharpl.22@uom.lk>
2025-05-04 23:56:08 +0530

    Re create settings page isue

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import UserSettings from "./pages/UserSettings/UserSettings";
-import Sidebar from "../src/components/sidebar/sidebar";
-import Footer from "../src/components/footer/Footer";
+
 
 function App() {
   return (
-    <Router>
-      <div style={{ display: "flex", flexDirection: "row", minHeight: "100vh" }}>
-        {/* Sidebar */}
-        <Sidebar />
-
-        {/* Main content */}
-        <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <div style={{ flex: 1 }}>
-            <Routes>
-              <Route path="/" element={<UserSettings />} />
-            </Routes>
-          </div>
-
-          {/* Footer */}
-          <Footer />
-        </div>
-      </div>
-    </Router>
+    
+      <BrowserRouter>
+        <Routes>
+      
+          <Route path="/settings" element={<UserSettings />} />
+      
+        </Routes>
+      </BrowserRouter>
+    
   );
 }
 
