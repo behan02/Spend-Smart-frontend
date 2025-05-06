c7f307dcfd3dc2b78e8630e0b26d82f397c6a7f1
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-05-06 13:21:20 +0530

    add admin login and connect to the backend

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,9 @@ function App() {
         <Route path="/report" element={<ReportOverview />} />
         <Route path="/goals" element={<Goals />} />{/* Add the route for Goals page */}
         <Route path="/transaction" element={<Transaction />} />
+
+        {/* Add the route for Admin Site*/}
+        <Route path="/admin/login" element={<AdminLogin />} />
         
       </Routes>
     </Router>
