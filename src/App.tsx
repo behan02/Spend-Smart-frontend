676d33d95e5629e84a3f60ef3221680143bbf8de
Samadhi Ayodhya Gamaarachchi <samadhigamaarachchi07@gmail.com>
2025-07-17 15:53:32 +0530

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import Sidebar from "./components/sidebar/sidebar";
-import UserLogin from "./pages/UserLogin/UserLogin";
-import UserRegister from "./pages/UserRegister/UserRegister";
+
 
 function App() {
   return (
     <Router>
       <Sidebar />
-      <Routes>
-        <Route path="/" element={<UserLogin />} />
-        <Route path="/register" element={<UserRegister />} />{" "}
-        {/* Add the route for Goals page */}
-      </Routes>
+      
     </Router>
   );
 }
